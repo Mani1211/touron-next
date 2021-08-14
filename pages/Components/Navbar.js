25eb987903089647e5115d02bb6bb2686adcb6f6
touron-travel <manivasagam.shanmugamtnj@gmail.com>
2021-08-14 18:40:31 +0530

    First commit

--- pages/Components/Navbar.js
+++ pages/Components/Navbar.js
@@ -35,7 +35,7 @@ const Navbar = ({ isOpen }) => {
   //   }, []);
   return (
     <div>
-      <nav className="flex justify-between px-10 items-center py-3 border-b shadow-md">
+      <nav className="md:flex justify-between px-10 items-center py-3 border-b shadow-md">
         {/* <div className="hamburger" onClick={handleClick}>
           <i className={clicked ? "fas fa-times" : "fas fa-bars"}></i>
         </div> */}
@@ -43,7 +43,7 @@ const Navbar = ({ isOpen }) => {
           <Image src="/logof.png" layout="fill" objectFit="contain" />
         </div>
 
-        <ul className="flex justify-evenly  items-center space-x-4">
+        <ul className="md:flex justify-evenly  items-center md:space-x-4">
           <li>Home</li>
           <li>About</li>
           <li>Contact</li>
